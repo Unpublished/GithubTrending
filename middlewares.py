5d3db687844ee19f069b70372a0fb7c913f92e8e
Unpublished <unpublished@gmx.net>
2018-03-15 19:43:52 +0100

    fix duplicated retry reason

--- middlewares.py
+++ middlewares.py
@@ -1,10 +1,12 @@
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
+from scrapy.http import TextResponse
 
 
 class CustomRetryMiddleware(RetryMiddleware):
     def process_response(self, request, response, spider):
         process_response = super().process_response(request, response, spider)
-        repo_list = response.css(".repo-list")
-        if repo_list.__len__() == 0:
-            return self._retry(request, 'dissect', spider)
+        if isinstance(process_response, TextResponse):
+            repo_list = process_response.css(".repo-list")
+            if repo_list.__len__() == 0:
+                return self._retry(request, 'dissect', spider) or response
         return process_response

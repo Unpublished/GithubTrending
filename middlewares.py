46d9f78c3d80779e6c8bcfd806e37380caccbfb5
Unpublished <unpublished@gmx.net>
2018-03-18 11:41:42 +0100

    On dissect retry after 45 secs (only if we haven't reached max retry count yet)

--- middlewares.py
+++ middlewares.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
 from scrapy.http import TextResponse
 
@@ -7,6 +9,7 @@ class CustomRetryMiddleware(RetryMiddleware):
         process_response = super().process_response(request, response, spider)
         if isinstance(process_response, TextResponse):
             repo_list = process_response.css(".repo-list")
-            if repo_list.__len__() == 0:
-                return self._retry(request, 'dissect', spider) or response
+            if repo_list.__len__() == 0 and not spider.crawler.stats.get_value('retry/max_reached', 0):
+                sleep(45)
+                return self._retry(request, 'dissect', spider) or process_response
         return process_response
